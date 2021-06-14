e8f7c3d775fbcbacd5c966c183f52ddd0077de2c
Dan Uhl <duhl@wayfair.com>
2021-06-14 13:57:41 -0400

    updates for use with node

--- scratchpad/bound-range-wrap.js
+++ scratchpad/bound-range-wrap.js
@@ -1,4 +1,4 @@
-function boundRangeWrap(lower, upper, num) {
+export default function boundRangeWrap(lower, upper, num) {
   // bound between two numbers (inclusive) wrapping around when going beyond limits
   // first shift everything from upper->lower to (0->upper-lower)
   const upperShifted = upper - lower + 1; // plus 1 for "inclusive" to upper bound
@@ -8,8 +8,8 @@ function boundRangeWrap(lower, upper, num) {
   return result + lower;
 }
 
-console.log(boundRangeWrap(5, 12, 4)); // 12
-console.log(boundRangeWrap(5, 12, 15)); // 7
-console.log(boundRangeWrap(5, 12, -5)); // 11
-console.log(boundRangeWrap(5, 12, 5)); // 5
-console.log(boundRangeWrap(5, 12, 12)); // 12
+// console.log(boundRangeWrap(5, 12, 4)); // 12
+// console.log(boundRangeWrap(5, 12, 15)); // 7
+// console.log(boundRangeWrap(5, 12, -5)); // 11
+// console.log(boundRangeWrap(5, 12, 5)); // 5
+// console.log(boundRangeWrap(5, 12, 12)); // 12

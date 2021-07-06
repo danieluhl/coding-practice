9ded2e8a91a4ce2969e63dd823be65cdad27b336
Dan Uhl <duhl@wayfair.com>
2021-07-06 07:28:05 -0400

    adds anagram

---
+++ exercism/javascript/rectangles/rectangles.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Rectangles' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Rectangles {
+  static count(input) {
+    if (input.length === 0) {
+      return 0;
+    }
+  }
+}

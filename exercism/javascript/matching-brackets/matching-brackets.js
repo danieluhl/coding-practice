5751de96d8c7a262d8e3c7d4c3a175a2d345a142
Dan Uhl <duhl@wayfair.com>
2021-07-20 21:18:50 -0400

    so good

--- exercism/javascript/matching-brackets/matching-brackets.js
+++ exercism/javascript/matching-brackets/matching-brackets.js
@@ -3,18 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
-const actionMap = {
-  '(': (stack) => stack.push('('),
-  '[': (stack) => stack.push('['),
-  '{': (stack) => stack.push('{'),
-  ')': (stack) => stack.pop() === '(',
-  ']': (stack) => stack.pop() === '[',
-  '}': (stack) => stack.pop() === '{',
+const expectedMap = { '{': '}', '(': ')', '[': ']' };
+
+module.exports = (stream) => {
+  let stack = [];
+  for (let token of stream) {
+    if (token in expectedMap) {
+      stack.push(expectedMap[token]);
+    } else if (stack.pop() !== token) {
+      return false;
+    }
+  }
+  return !stack.length;
 };
 
-export const isPaired = (str, stack = []) =>
-  [...str].every((next) => !actionMap[next] || actionMap[next](stack)) &&
-  stack.length === 0;
+// const actionMap = {
+//   '(': (stack) => stack.push('('),
+//   '[': (stack) => stack.push('['),
+//   '{': (stack) => stack.push('{'),
+//   ')': (stack) => stack.pop() === '(',
+//   ']': (stack) => stack.pop() === '[',
+//   '}': (stack) => stack.pop() === '{',
+// };
+
+// export const isPaired = (str, stack = []) =>
+//   [...str].every((next) => !actionMap[next] || actionMap[next](stack)) &&
+//   stack.length === 0;
 
 // const itemMap = {
 //   '(': 1,

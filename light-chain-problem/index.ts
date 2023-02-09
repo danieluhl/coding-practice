0c512163eb9cce4e3195da9ca159a4985a8f17b4
Dan Uhl <duhl@wayfair.com>
2023-02-08 23:14:19 -0500

    adds light chain problem

---
+++ light-chain-problem/index.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S ts-node --files
+
+const sample1 = [1, 3, 5, 2, 4];
+const sample2 = [1, 2, 3];
+const sample3 = [3, 2, 1];
+const getTotalCost = (lightString: number[]) => {
+  // loop over each, if not next, add to a set
+  // if is next, also pop from set
+  let time = lightString.length;
+  const big = new Set();
+  let next = 1;
+  let total = 0;
+  for (let i = 0; i < lightString.length; i++) {
+    if (lightString[i] === next) {
+      total = total + time;
+      next = next + 1;
+      while (big.has(next)) {
+        big.delete(next);
+        next = next + 1;
+        total = total + time;
+      }
+    } else {
+      big.add(lightString[i]);
+    }
+    time = time - 1;
+  }
+  return total;
+};
+
+console.log(getTotalCost(sample1));
+console.log(getTotalCost(sample2));
+console.log(getTotalCost(sample3));
